47eed2190116d02335570ed71b44a92ed0eed1fe
AD\akashprajapati <akashprajapati>
2023-07-03 12:08:00 +0530

    autowiring, standalone_collections, BeanScope, annotations

--- src/main/java/com/akki/collections/Employee.java
+++ src/main/java/com/akki/collections/Employee.java
@@ -8,6 +8,9 @@ public class Employee {
         ApplicationContext context = new ClassPathXmlApplicationContext("configcollection.xml");
         Emp emp = (Emp) context.getBean("emp1");
         System.out.println(emp);
+        System.out.println(emp.getPhones());    //to view list of phone numbers
+        System.out.println(emp.getPhones().getClass().getName());   // to check which list is this? i mean ArrayList, Vector, LinkedList etc.
+
         Emp anotherEmp = (Emp) context.getBean("emp2");
         System.out.println(anotherEmp);
     }
